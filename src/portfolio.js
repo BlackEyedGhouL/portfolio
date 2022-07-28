32466f6eb78c93a77408aaa7cb1bf5628129918e
BlackEyedGhouL <34senith@gmail.com>
2022-07-28 08:34:43 +0530

    Initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,126 @@
+const header = {
+  homepage: "",
+  title: "SU.",
+};
+
+const about = {
+  name: "Senith Umesha",
+  role: "Mobile Developer",
+  description:
+    "I am a hardworking, flexible and self-motivated individual with 7 months of experience in mobile development, using Flutter. I am an excellent team worker. And also I am able to work under pressure and adhere to strict deadlines.",
+  resume:
+    "https://drive.google.com/file/d/17dDSKR4l8SoFjVmlxcnDAFCPtd3ViyXh/view?usp=sharing",
+  social: {
+    linkedin: "https://www.linkedin.com/in/senith-umesha/",
+    github: "https://github.com/BlackEyedGhouL",
+  },
+};
+
+const projects = [
+  {
+    name: "Ayu",
+    description:
+      "Ayu is a mobile telemedicine app that helps both patients and doctors with channeling, consulting and a lot more. It has 3 applications, an admin web app and 2 mobile apps on the patient and doctor sides.",
+    stack: ["Flutter", "Firebase"],
+    sourceCode: "",
+    livePreview: "",
+  },
+  {
+    name: "Kokiya",
+    description:
+      "Kokiya is a mobile application that makes cooking easier, faster and more convenient. Users have the ability to interact with others and share their culinary experiences with them.",
+    stack: ["Flutter", "Firebase"],
+    sourceCode: "",
+    livePreview: "",
+  },
+  {
+    name: "Fire Rescue",
+    description:
+      "Make the life of firefighters easier by implementing features like details and locations of all stations, contact information of all fellow firefighters, instant alerts, information about upcoming events, currently available positions.",
+    stack: ["Java", "SQLite", "Firebase"],
+    sourceCode: "https://github.com/BlackEyedGhouL/fire-rescue",
+    livePreview: "",
+  },
+  {
+    name: "Covid Zero",
+    description:
+      "Provides features like the latest information about the current situation of COVID-19 virus by using APIs and visualizing data using charts, locations where users can get vaccinated, an online vaccine registration service, a self-isolation countdown timer and the latest news about the virus.",
+    stack: ["Java", "SQLite", "Firebase"],
+    sourceCode: "https://github.com/BlackEyedGhouL/covid-zero",
+    livePreview: "",
+  },
+  {
+    name: "CoChat",
+    description:
+      "Chat application developed for messaging between small groups of friends. Allows direct messaging, phone authentication, multiple chat backgrounds and inviting friends.",
+    stack: ["Kotlin", "Firebase"],
+    sourceCode: "https://github.com/BlackEyedGhouL/co-chat",
+    livePreview: "",
+  },
+  {
+    name: "ATM System",
+    description:
+      "A desktop application that lets users access services like checking account details, withdrawing / depositing money, information about nearby ATMs and fund transfer. Also provides features like monitoring user activity and keeping user data secure by an instant email alert service.",
+    stack: ["C#", "HTML", "CSS", "MSSQL"],
+    sourceCode: "https://github.com/BlackEyedGhouL/atm-system",
+    livePreview: "",
+  },
+  {
+    name: "College LMS",
+    description:
+      "A learning management system to help students acquire a better learning experience by using an in-build library system, past exam paper system, details about all available courses and more features.",
+    stack: ["C#", "MSSQL"],
+    sourceCode: "https://github.com/BlackEyedGhouL/zmc-academy",
+    livePreview: "",
+  },
+  {
+    name: "Warehouse Manager",
+    description:
+      "Allows users to select a transport method and transfer their wares between our facilities around the country at any time. And also the application lets them manage their warehouse inventory.",
+    stack: ["Java", "MySQL"],
+    sourceCode: "https://github.com/BlackEyedGhouL/warehouse-manager",
+    livePreview: "",
+  },
+  {
+    name: "Library System",
+    description:
+      "A web application that aims to create a computerized library system to reduce manual work. I've created two microservices with separate databases with a user-friendly UI. The system lets users get all the information about books available in the library and search them by their authors and names.",
+    stack: ["Java", "JSP", "Spring Boot", "Bootstrap", "MySQL"],
+    sourceCode: "https://github.com/BlackEyedGhouL/library-system-ms",
+    livePreview: "",
+  },
+];
+
+const skills = [
+  "Android",
+  "Flutter",
+  "Dart",
+  "Kotlin",
+  "Git",
+  "Firebase",
+  "MSSQL",
+  "MySQL",
+  "SQLite",
+  "C",
+  "C++",
+  "C#",
+  "Java",
+  "Python",
+  "Flask",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Spring",
+  "Postman",
+  "Matlab",
+  "Photoshop",
+  "Arduino",
+  "Linux",
+];
+
+const contact = {
+  email: "umesha.pms@gmail.com",
+};
+
+export { header, about, projects, skills, contact };
